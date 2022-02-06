9ba1189b4d120f660a1d04370ad50b226a3cc80b
Akhil Chandra <87477447+AChandra5@users.noreply.github.com>
2022-02-06 19:39:39 +0530

    Add files via upload

---
+++ toDo.js
@@ -0,0 +1,23 @@
+document.getElementById("addTodo").addEventListener("click", () => {
+    const input = document.getElementById("inputText")
+    const todoItem = input.value;
+
+    if (!todoItem || todoItem.length <= 0) return;
+
+    createTodoItem(todoItem)
+
+    input.value = "";
+});
+
+
+const createTodoItem = (textValue) => {
+
+    const element = `
+<div class="alert alert-danger alert-dismissible fade show" role="alert" style="width: 90vw;">
+${textValue}
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
+
+    document.getElementById('todoContainer').innerHTML += element
+}
